d6c71544b25a8dbbea15ec1dc40402e57eb56e01
CodeBaraa <barbara.hajduk12@gmail.com>
2026-04-22 15:30:51 +0200

    Initial commit with clean gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
   base: './', // Wichtig für Netlify Drop
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        impresszum: resolve(__dirname, 'impresszum.html'),
-        adatkezeles: resolve(__dirname, 'adatkezeles.html'),
-        aszf: resolve(__dirname, 'aszf.html'),
+        impressum: resolve(__dirname, 'impressum.html'),
+        datenschutz: resolve(__dirname, 'datenschutz.html'),
+        agb: resolve(__dirname, 'agb.html'),
       },
     },
   },
